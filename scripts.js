3cd7bcf82a7a9d7cd3f928f410daa943cbc00912
stuydw <dw@stuy.edu>
2021-06-15 20:08:59 -0400

    + lightsaber

--- scripts.js
+++ scripts.js
@@ -43,6 +43,19 @@ var teru_stats = {
 };
 
 var weapons = {
+  'dual_saber' : {
+    name: 'Dual Bladed Saber',
+    type: 'lightsaber',
+    crit: 2,
+    accurate: 0,
+    pierce: 0,
+    damage: 6,
+    breach: 1,
+    linked: 1,
+    sunder: 1,
+    unweildy: 3
+  },
+
   'training_saber' : {
     name: 'Training Saber',
     type: 'lightsaber',
